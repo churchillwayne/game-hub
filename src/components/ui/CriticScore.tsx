69e4fa28a4d8993d46910b81e9e4538781bc9a1f
Wayne Churchill <Waynec@informbilling.co.uk>
2025-08-07 12:15:16 +0100

    Displaying Critic Score

---
+++ src/components/ui/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let colour = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge
+      colorPalette={colour}
+      fontSize="14px"
+      paddingRight={2}
+      borderRadius="4px"
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
